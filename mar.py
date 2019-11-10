87fed9453288a1c9d32426b9ac74108951d0dbdf
Piero Ocampo <piero.ocampo@gmail.com>
2019-11-10 09:17:49 -0600

    Exponential backoff and school resolving

--- mar.py
+++ mar.py
@@ -1,4 +1,6 @@
 import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 import pandas as pd
 
 def query(addr):
@@ -10,20 +12,23 @@ def query(addr):
 
 
 def query_schools(x,y):
+	max_attempts = 10
+	attempts = 0
+	s = requests.Session()
 	url = 'http://geospatial.dcgis.dc.gov/SchoolsWebService/sy19-20/getSchools.asmx/findSchoolsNew?x={}&y={}&f=json'.format(x,y)
-	r = requests.get(url)
-	r.raise_for_status()
+	retries = Retry(total=5, backoff_factor = 1, status_forcelist=[500, 502, 503, 504])
+	s.mount('http://', HTTPAdapter(max_retries=retries))
+	r = s.get(url)
 	return r.json()
 
 
 def add_schools(row):
 	schools = query_schools(row['XCOORD'],row['YCOORD'])
-	row['elementary_school'] = schools['newBoundarySchools']['inBoundaryElementarySchool']
-	row['middle_school'] = schools['newBoundarySchools']['inBoundaryMiddleSchool']
-	row['high_school'] = schools['newBoundarySchools']['inBoundaryHighSchool']
-	return row
+	return pd.Series([schools['newBoundarySchools']['inBoundaryElementarySchool'], 
+					schools['newBoundarySchools']['inBoundaryMiddleSchool'], 
+					schools['newBoundarySchools']['inBoundaryHighSchool']])
 
 if __name__ == "__main__":
-	df = pd.read_csv('Address_Points.csv')
-	df.apply(add_schools, axis=1)
+	df = pd.read_csv('Address_Points.csv')	
+	df[['elementary_school', 'middle_school', 'high_school']] = df.apply(add_schools, axis=1)
 	df.to_csv(r'Address_Points_With_Schools.csv')
